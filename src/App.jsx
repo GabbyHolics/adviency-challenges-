749bb76564e0abe0f52793399f9193f818ec4da8
GabbyHolics <karenthgabriela@gmail.com>
2021-12-03 10:27:36 -0300

    day 2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <ul className="list-container">
+           <h1 className="h1">Regalos</h1>
+            <li className="lista">Zapatos</li>
+            <li className="lista">Cartera</li>
+            <li className="lista">Casa</li>
+            
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
